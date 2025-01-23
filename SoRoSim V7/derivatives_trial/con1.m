2b2ac9edd2620fb4475bdd181b6891c6a8f19af3
AneesPeringal <anees870@gmail.com>
2025-01-23 10:44:06 +0400

    staticresidue_jacobian update

--- SoRoSim V7/derivatives_trial/con1.m
+++ SoRoSim V7/derivatives_trial/con1.m
@@ -3,8 +3,7 @@ q = x(1:Linkage.ndof);
 u = x(Linkage.ndof+1:Linkage.ndof+12);
 l = x(Linkage.ndof+13:Linkage.ndof+18);
 
-[res, jacobian] = Equilibrium(Linkage,[q;l],u,1);
+[res, jac_q, jac_u, jac_l] = StaticResidueJacobian(Linkage, q, u, l);
 con1 = res;
-dcon1 = jacobian;
-
+dcon1 = [jac_q, jac_u, jac_l];
 end
