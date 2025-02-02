5f3d6a21c258b81ed5a537fe2818751fdf2399a6
AneesPeringal <anees870@gmail.com>
2025-02-03 01:54:30 +0400

    Examples verified
    Various desired pose is attempted with the hole constraints

--- SoRoSim V7/Planning_scripts/constrained_path_planning.m
+++ SoRoSim V7/Planning_scripts/constrained_path_planning.m
@@ -5,6 +5,7 @@ load("Datafiles\Parallel_robot.mat");
 S1.VLinks(1).ld = {0.9};
 S1.VLinks(3).ld = {0.9};
 S1.VLinks(1).E = 5.105e+7;
+S1.VLinks(2).Rho = 2240;
 S1.VLinks(3).E = 5.105e+7;
 S1.PlotParameters.ClosePrevious = false;
 S1.CVRods{1}(2).UpdateAll;
@@ -12,9 +13,9 @@ S1.CVRods{3}(2).UpdateAll;
 S1 = S1.Update;
 
 %%
-g_des_final = [0.0000         0   1.0000    0.3
-                0    1.0000         0         0
-                -1.0000         0    0.0000    -0.3
+g_des_final = [0.0000         0   1.0000    0.1
+                0    1.0000         0         0.2
+                -1.0000         0    0.0000    -0.4
                 0         0         0    1.0000];
 
 g_des_initial = [0.0000         0   1.0000    0.3
@@ -26,6 +27,9 @@ rotation_angle = 15;
 rotation_angle = rotation_angle*(pi/180);
 roty = eul2tform([0, rotation_angle,0]);
 =======
+rotation_angle = 20;
+rotation_angle = rotation_angle*(pi/180);
+roty = eul2tform([0,0, rotation_angle]);
 >>>>>>> Stashed changes
 g_des_final = g_des_final*roty;
 
@@ -46,6 +50,8 @@ S1.plotq(qu_uq_l_final1(1:S1.ndof));
 S1.plotq(qu_uq_l_final2(1:S1.ndof));
 [constraint_surface, root1, root2] = find_constraint(S1, qu_uq_l_final1(1:S1.ndof), 0);
 plot_constraint(constraint_surface);
+plotTransforms(se3(g_des_initial), 'FrameSize',0.05);
+plotTransforms(se3(g_des_final), 'FrameSize',0.05);
 %% Problem 2
 % This problem requires finding the pose of the two ends of the robot, such
 % that the rigid body achieves a certain desired pose while satisfying the
@@ -57,6 +63,7 @@ constraints_handle = @(qu_uq_l)Constraints2(S1, qu_uq_l, constraint_surface);
 <<<<<<< Updated upstream
 options = optimoptions('fmincon','Display','iter','OptimalityTolerance',1e-10,'StepTolerance',1e-15 ,'MaxFunctionEvaluations',2e6,'Algorithm', 'sqp', 'SpecifyObjectiveGradient',true, 'SpecifyConstraintGradient',true);%,'EnableFeasibilityMode',true);%,'OptimalityTolerance',1e-10,'StepTolerance',1e-20);
 =======
+options = optimoptions('fmincon','Display','iter','OptimalityTolerance',1e-6,'StepTolerance',1e-8 ,'MaxFunctionEvaluations',2e10,'Algorithm', 'sqp', 'SpecifyObjectiveGradient',true, 'SpecifyConstraintGradient',true);%,'EnableFeasibilityMode',true);%,'OptimalityTolerance',1e-10,'StepTolerance',1e-20);
 >>>>>>> Stashed changes
 lb = zeros(80,1);
 ub = zeros(80,1);
@@ -84,6 +91,7 @@ S1.plotq(qu_uq_l_final2(1:S1.ndof))
 hold on
 plot_constraint(constraint_surface)
 plotTransforms(se3(g_des_initial),'FrameSize',0.05)
+plotTransforms(se3(g_des_final), 'FrameSize',0.05)
 
 
 %% Problem 3: Path planning to go from initial desired end effector to final
@@ -106,12 +114,15 @@ constraints_handle = @(qu_uq_l)Constraint3(S1, qu_uq_l, n_points, g_des_initial)
 options = optimoptions('fmincon','Display','iter','OptimalityTolerance',1e-6,'StepTolerance',1e-6 ,'MaxFunctionEvaluations',2e10,'Algorithm','sqp','SpecifyObjectiveGradient',true,'SpecifyConstraintGradient',true);%EnableFeasibilityMode',true);%,'OptimalityTolerance',1e-10,'StepTolerance',1e-20);
 =======
 % lambda = 5e3;
+constraints_handle = @(qu_uq_l)Constraint3(S1, qu_uq_l, n_points, g_des_initial, g_des_final);
+options = optimoptions('fmincon','Display','iter','OptimalityTolerance',1e-10,'StepTolerance',1e-10 ,'MaxFunctionEvaluations',2e10,'Algorithm','sqp','SpecifyObjectiveGradient',true,'SpecifyConstraintGradient',true);%EnableFeasibilityMode',true);
 >>>>>>> Stashed changes
 tic
+qu_uq_l_con = fmincon(@(qu_uq_l)Objective4(S1, qu_uq_l, n_points),initial_guess, [],[],[],[],[],[],constraints_handle,options);
 toc
 save("Datafiles/zy_rotation_neg15_degs.mat","qu_uq_l_con")
 %%
-objective_function(S1, qu_uq_l_con,g_des_final)
+Objective4(S1, qu_uq_l_con,g_des_final, 10)
 %% Plot the solution
 % figure
 <<<<<<< Updated upstream
@@ -119,7 +130,43 @@ S1.PlotParameters.ClosePrevious= false;
 qu_uq_l_con = reshape(qu_uq_l_con, n_x_t, n_points);
 qu_uq_l_con = qu_uq_l_con';
 qs = qu_uq_l_con(:,1:S1.ndof);
+=======
+
+S1.PlotParameters.ClosePrevious= false;
+qu_uq_l = reshape(qu_uq_l_con, n_x_t, n_points);
+qu_uq_l = qu_uq_l';
+qs = qu_uq_l(:,1:S1.ndof);
+>>>>>>> Stashed changes
 gs1 = S1.FwdKinematics(qs(1,:));
+plot_constraint(constraint_surface)
+for i = 1:10
+    S1.plotq(qs(i,:))
+    hold on
+end
+plotTransforms(se3(g_des_final),'FrameSize',0.08)
+
+%% Make a good initial condition by interpolating the desired pose and solving at each instance
+n_points = 10;
+n_x_t = 80;
+g = interpolate_transformation(g_des_initial, g_des_final, n_points);
+qu_uq_l = [qu_uq_l_final1; root1; root2];
+
+constraints_handle = @(qu_uq_l)Constraints2(S1, qu_uq_l, constraint_surface);
+options = optimoptions('fmincon','Display','final-detailed','OptimalityTolerance',1e-6,'StepTolerance',1e-8 ,'MaxFunctionEvaluations',2e10,'Algorithm', 'sqp', 'SpecifyObjectiveGradient',true, 'SpecifyConstraintGradient',true);%,'EnableFeasibilityMode',true);%,'OptimalityTolerance',1e-10,'StepTolerance',1e-20);
+lb = zeros(80,1);
+ub = zeros(80,1);
+lb(1:78) = -inf;
+lb(79:80) = 0+0.1;
+ub(1:78) = inf;
+ub(79:80) = 1-0.1;
+qu_uq_l_final = zeros(n_points, n_x_t);
+for i=1:n_points
+    qu_uq_l = fmincon(@(qu_uq_l)Objective2(S1, qu_uq_l, g(4*i-3:4*i,:)), qu_uq_l_final(i,:)', [],[],[],[],lb,ub,constraints_handle,options);
+    qu_uq_l_final(i,:) = qu_uq_l;
+    Objective2(S1,qu_uq_l, g(4*i-3:4*i,:))
+end
+%%
+qs = qu_uq_l_final(:,1:S1.ndof);
 plot_constraint(constraint_surface)
 for i = 1:5
     S1.plotq(qs(2*i,:))
@@ -168,6 +215,22 @@ qu_uq_l_con = reshape(qu_uq_l_con, n_x_t, n_points);
 qu_uq_l_con = qu_uq_l_con';
 qs = qu_uq_l_con(:,1:S1.ndof);
 gs1 = S1.FwdKinematics(qs(1,:));
+=======
+constraints_handle = @(qu_uq_l)Constraints4(S1, qu_uq_l, n_points, g_des_initial, g_des_final, constraint_surface);
+options = optimoptions('fmincon','Display','iter','OptimalityTolerance',1e-6,'StepTolerance',1e-8 ,'MaxFunctionEvaluations',2e10,'Algorithm','sqp','SpecifyObjectiveGradient',true,'SpecifyConstraintGradient',true);%EnableFeasibilityMode',true);%,'OptimalityTolerance',1e-10,'StepTolerance',1e-20);
+tic
+qu_uq_l_con = fmincon(@(qu_uq_l)Objective4(S1, qu_uq_l, n_points),initial_guess, [],[],[],[],lb,ub,constraints_handle,options);
+toc
+%%
+Objective4(S1, qu_uq_l_con,g_des_initial, n_points)
+%%
+figure
+S1.PlotParameters.ClosePrevious= false;
+qu_uq_l = reshape(qu_uq_l_con, n_x_t, n_points);
+qu_uq_l = qu_uq_l';
+%%
+qs = qu_uq_l_final(:,1:S1.ndof);
+>>>>>>> Stashed changes
 plot_constraint(constraint_surface)
 for i = 1:5
     S1.plotq(qs(2*i,:))
