9831808ae3649dba0856d7fe10b46733dd4b7e39
Daniele Caradonna <danele.caradonna@gmail.com>
2026-05-04 10:13:21 +0200

    Update ActuationMatrix.m

--- Differentiable_SoRoSim/SorosimLinkage_files/@SorosimLinkage/ActuationMatrix.m
+++ Differentiable_SoRoSim/SorosimLinkage_files/@SorosimLinkage/ActuationMatrix.m
@@ -1,190 +1,109 @@
-%Function that calculates the generalized actuation matrix (Bq) at every
-%Last modified by Anup Teejo Mathew 02.03.2022
+%Function that calculates the generalized actuation matrix (Bq)
+%Inspired by dynamicsSolver and updated to match SorosimLinkage properties
+function Bq = ActuationMatrix(Linkage,q)
 
-function Bq = ActuationMatrix(Tr,q)
-
-if isrow(q)
-    q=q';
-end
-
-if Tr.Actuated
-    
-    J              = Tr.Jacobian(q);
-    nact           = Tr.nact;
-    ndof           = Tr.ndof;
-    Bq             = zeros(ndof,nact);
-    
-    %revolute, prismatic, helical joints
-    Bqj1           = Tr.Bqj1;
-    n_1dof         = size(Bqj1,2);
-    Bq(:,1:n_1dof) = Tr.Bqj1;
-    
-    %for other joints
-    i_jact  = Tr.i_jact;
-    i_u     = n_1dof+1;
-    i_jactq = Tr.i_jactq;
-    
-    n_ljact = length(i_jact);
-    for iii=n_1dof+1:n_ljact
-        i = i_jact(iii);
-        i_sig = 1;
-        dof_start = 1;
-        for ii=1:i-1
-            i_sig = i_sig+1;
-            dof_start = dof_start+Tr.CVTwists{ii}(1).dof; %joint
-            for j=1:Tr.VLinks(Tr.LinkIndex(ii)).npie-1
-                i_sig = i_sig+Tr.CVTwists{ii}(j+1).nip;
-                dof_start = dof_start+Tr.CVTwists{ii}(j+1).dof;
-            end
-            if Tr.VLinks(Tr.LinkIndex(ii)).linktype=='r'
-                i_sig = i_sig+1;
-            end
-        end
-
-        if Tr.VLinks(Tr.LinkIndex(i)).jointtype=='C'
-            dof_here = dof_start+Tr.CVTwists{i}(1).dof;
-            Bq(i_jactq(i_u:i_u+dof_here-1),i_u:i_u+dof_here-1) = [1 0;0 1];
-            i_u = i_u+2;
-        else 
-            dof_here = Tr.CVTwists{i}(1).dof;
-            J_here = J((i_sig-1)*6+1:i_sig*6,:);
-            S_here = J_here(:,i_jactq(i_u:i_u+dof_here-1));
-            B_here = Tr.CVTwists{i}(1).B;
-
-            Bq(i_jactq(i_u:i_u+dof_here-1),i_u:i_u+dof_here-1) = S_here'*B_here;
-            i_u = i_u+dof_here;
-        end
+    if isrow(q)
+        q = q';
     end
-    
-    %cable actuation
-    n_sact = Tr.n_sact;
-    N      = Tr.N;
-    for iso=1:n_sact
+
+    if Linkage.Actuated
         
-        dcii = cell(1,N); dcpii = cell(1,N); Sdivii = zeros(N,1); Edivii = zeros(N,1);  
-        for i=1:N
-            dcii{i}   = Tr.dc{iso,i};
-            dcpii{i}  = Tr.dcp{iso,i};
-            Sdivii(i) = Tr.Sdiv(iso,i);
-            Edivii(i) = Tr.Ediv(iso,i);
+        J    = Linkage.Jacobian(q);
+        nact = Linkage.nact;
+        ndof = Linkage.ndof;
+        Bq   = zeros(ndof,nact);
+        
+        N_jact = Linkage.N_jact; % number of Links whose joints are actuated
+        n_jact = Linkage.n_jact; % total number of joint actuators
+        
+        %% 1. Rigid/Joint Actuation
+        % revolute, prismatic, helical joints (1D joints)
+        Bj1   = Linkage.Bj1; 
+        na_j1 = size(Bj1,2); 
+        if na_j1 > 0
+            Bq(:,1:na_j1) = Bj1;
         end
-        Insideii      = Tr.Inside(iso);
-
-        if Insideii
+        
+        % for other multi-DOF joints
+        i_jact  = Linkage.i_jact;
+        i_u     = na_j1 + 1;
+        i_jactq = Linkage.i_jactq;
+        
+        for ii = na_j1+1:N_jact
+            i = i_jact(ii);
+            dof_here = Linkage.CVRods{i}(1).dof;
+            us_here  = i_u : i_u+dof_here-1;
+            dofs_here = i_jactq(us_here);
             
+            if Linkage.VLinks(Linkage.LinkIndex(i)).jointtype == 'C'
+                Bq(dofs_here, us_here) = eye(2);
+            else 
+                % Utilizing ActuationPrecompute as done in dynamicsSolver
+                i_sig = Linkage.ActuationPrecompute.i_sig_act(ii); 
+                
+                if Linkage.VLinks(Linkage.LinkIndex(i)).linktype == 'r'
+                    gi = Linkage.VLinks(Linkage.LinkIndex(i)).gi;
+                else
+                    gi = Linkage.VLinks(Linkage.LinkIndex(i)).gi{1};
+                end
+                
+                AdgstepinvS_here = dinamico_Adjoint(gi) * J((i_sig-1)*6+1:i_sig*6, i_jactq(i_u:i_u+dof_here-1));
+                Phi_here = Linkage.CVRods{i}(1).Phi;
+                Bq(dofs_here, us_here) = AdgstepinvS_here' * Phi_here;
+            end
+            i_u = i_u + dof_here;
+        end
+        
+        %% 2. Soft/Cable Actuation
+        if Linkage.n_sact > 0
             dof_start = 1;
-
-            for i=1:Tr.N
-
-                dof_start = dof_start+Tr.CVTwists{i}(1).dof;
-
-                for j=1:Tr.VLinks(Tr.LinkIndex(i)).npie-1
-
-                    dof_here  = Tr.CVTwists{i}(j+1).dof;
-                    VTwists  = Tr.CVTwists{i};
-
-                    if j>=Sdivii(i)&&j<=Edivii(i)
-
-                        dcd  = dcii{i}{j};
-                        dcpd = dcpii{i}{j};
-
-                        q_here  = q(dof_start:dof_start+dof_here-1);
-                        xi_star = VTwists(j+1).xi_star;
-                        ld      = Tr.VLinks(Tr.LinkIndex(i)).lp{j};
-                        Ws      = VTwists(j+1).Ws;
-                        nip     = VTwists(j+1).nip;
-
-                        %scaling of quantities
-                        Lscale       = ld;
-
-                        for k=1:nip
-                            if Ws(k)>0
-                                dc_here     = dcd(:,k);
-                                dcp_here    = dcpd(:,k);
-                                xi_here = xi_star(6*(k-1)+1:6*k,1);
-                                xi_here(1:3) = xi_here(1:3)*Lscale; %scaling using the formula: Lscale m = 1 unit
-
-                                dBqdq_here = VTwists(j+1).B((k-1)*6+1:k*6,:);
-                                if dof_here>0
-                                    xi_here    = dBqdq_here*q_here+xi_here;
+            for i = 1:Linkage.N
+                
+                dof_here = Linkage.CVRods{i}(1).dof;
+                if ~Linkage.OneBasis
+                    dof_start = dof_start + dof_here;
+                end
+                
+                for j = 1:Linkage.VLinks(Linkage.LinkIndex(i)).npie-1
+                    na_here   = length(Linkage.i_sact{i}{j});
+                    dof_here  = Linkage.CVRods{i}(j+1).dof;
+                    dofs_here = dof_start : dof_start+dof_here-1;
+                    
+                    if ~Linkage.OneBasis
+                        dof_start = dof_start + dof_here;
+                    end
+                    
+                    if na_here > 0
+                        Ws  = Linkage.CVRods{i}(j+1).Ws;
+                        nip = Linkage.CVRods{i}(j+1).nip; % Defining nip locally!
+                        B_here = zeros(dof_here, na_here);
+                        
+                        for ii = 1:nip
+                            if Ws(ii) > 0
+                                Phi = Linkage.CVRods{i}(j+1).Phi((ii-1)*6+1:ii*6,:);
+                                xi  = Phi*q(dofs_here) + Linkage.CVRods{i}(j+1).xi_star((ii-1)*6+1:ii*6,1);
+                                
+                                Phi_a = zeros(6,na_here);
+                                xihat_123 = [0 -xi(3) xi(2) xi(4); xi(3) 0 -xi(1) xi(5); -xi(2) xi(1) 0 xi(6)];
+                                
+                                ia_here = 1;
+                                for ia = Linkage.i_sact{i}{j}
+                                    dc  = Linkage.dc{ia,i}{j}(:,ii);
+                                    dcp = Linkage.dcp{ia,i}{j}(:,ii);
+                                    Phi_a(:,ia_here) = SoftActuator_FD(dc, dcp, xihat_123);
+                                    ia_here = ia_here + 1;
                                 end
-                                xi_here(1:3) = xi_here(1:3)/Lscale; %unscaling
-                                xihat_here  = [0 -xi_here(3) xi_here(2) xi_here(4);xi_here(3) 0 -xi_here(1) xi_here(5);-xi_here(2) xi_here(1) 0 xi_here(6)];%4th row is avoided to speedup calculation
-                                tang        = xihat_here*[dc_here;1]+dcp_here;
-                                tang        = tang/norm(tang);
-                                Btau        = [[0 -dc_here(3) dc_here(2);dc_here(3) 0 -dc_here(1);-dc_here(2) dc_here(1) 0]*tang;tang];
-                                Btau(1:3)   = Btau(1:3)/Lscale;
-
-                                Bq(dof_start:dof_start+dof_here-1,Tr.n_jact+iso)  = Bq(dof_start:dof_start+dof_here-1,Tr.n_jact+iso)+Ws(k)*dBqdq_here'*Btau*Lscale; %scaled back for addition
+                                B_here = B_here + Ws(ii) * Phi' * Phi_a;
                             end
                         end
-
+                        % Map B_here into the full Bq matrix using n_jact offset
+                        Bq(dofs_here, n_jact + Linkage.i_sact{i}{j}) = B_here;
                     end
-                    dof_start = dof_start+dof_here;
-                end 
-            end
-        else
-            g = Tr.FwdKinematics(q);
-            i_sig     = 1;
-            dof_start = 1;
-            for i=1:Tr.N
-    
-                dof_start = dof_start+Tr.CVTwists{i}(1).dof;
-                i_sig    = i_sig+1;
-                if Tr.VLinks(Tr.LinkIndex(i)).linktype=='r'
-                    i_sig = i_sig+1;
                 end
-
-
-                for j=1:Tr.VLinks(Tr.LinkIndex(i)).npie-1
-
-                    dof_here = Tr.CVTwists{i}(j+1).dof; 
-                    nip = Tr.CVTwists{i}(j+1).nip;
-
-                    if j==Sdiv(i)
-                        dof_act_start = dof_start;
-                        dof_act = 0;
-                        i_sig_l = i_sig;
-                        dc_l = dcii{i}{j}(:,1);
-                    end
-                    if j==Ediv(i)
-                        i_sig_r = i_sig+nip-1;
-                        dc_r = dcii{i}{j}(:,end);
-                    end
-                    if j>=Sdiv(i)&&j<=Ediv(i)
-                        dof_act = dof_act+dof_here;
-                    end
-
-                    i_sig = i_sig+nip;
-                    dof_start = dof_start+dof_here;
-                end
-
-                ld       = Tr.VLinks(Tr.LinkIndex(i)).lp{Ediv(i)};
-                Lscale   = ld;
-
-                %scaling of quantities
-                dc_l       = dc_l/Lscale;
-                dc_r       = dc_r/Lscale;
-
-                g_leftinv = [eye(3) -dc_l;0 0 0 1];
-                g_right   = [eye(3) dc_r;0 0 0 1];
-
-                g_iL      = ginv(g((i_sig_l-1)*4+1:i_sig_l*4,:))*g((i_sig_r-1)*4+1:(i_sig_r)*4,:);
-                g_u       = g_leftinv*g_iL*g_right;
-                u         = g_u(1:3,4);
-                u         = u/sqrt(u'*u);
-
-                J_iL = J((i_sig_r-1)*6+1:(i_sig_r)*6,:);
-                Bq(dof_act_start:dof_act_start+dof_act-1,Tr.n_jact+iso) = J_iL(:,dof_act_start:dof_act_start+dof_act-1)'*dinamico_coAdjoint(g_right)*[0 0 0 u']'*Lscale; %scaling back 
-
             end
         end
         
+    else
+        Bq = 0;
     end
-    %add more type of actuations if needed
-    
-else
-    Bq = 0;
 end
-end
-
